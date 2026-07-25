8aefaeb470fe83f6b00561489984051876114627
ganeshadityas9 <ganeshaditya.k@gmail.com>
2026-07-26 00:58:11 +0530

    Added SNS notification in Consumer Lambda

--- lambda-consumer/app.py
+++ lambda-consumer/app.py
@@ -1,14 +1,21 @@
 import json
+import boto3
+
+sns = boto3.client('sns')
+
+TOPIC_ARN = "arn:aws:sns:us-east-1:272795261907:EC2AlertTopic"
+
+
+
 
 def lambda_handler(event, context):
-
     for record in event["Records"]:
-
         body = json.loads(record["body"])
 
         print(body)
 
-    return {
-        "statusCode": 200,
-        "body": "Messages Processed"
-    }
+        sns.publish(
+            TopicArn=TOPIC_ARN,
+            Subject="EC2 High CPU Alert",
+            Message=json.dumps(body, indent=2)
+        )
