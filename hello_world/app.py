625bf4154449d837ef96ac30a3e37ab2724a09f8
ganeshadityas9 <ganeshaditya.k@gmail.com>
2026-07-25 23:40:16 +0530

    Updated Message from Helloworldapp

--- hello_world/app.py
+++ hello_world/app.py
@@ -16,7 +16,7 @@ def lambda_handler(event, context):
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "hello world",
+            "message": "Hello Ganesh, Deployment Success",
             # "location": ip.text.replace("\n", "")
         }),
     }
